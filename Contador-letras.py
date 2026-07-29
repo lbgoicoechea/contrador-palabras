54ec025517630190b59ba57ab3470e844165ee1b
Lucila <lgoicoechea@itba.edu.ar>
2026-07-29 19:40:41 -0300

    realizamos contador de palabras

---
+++ Contador-letras.py
@@ -0,0 +1,12 @@
+
+# Paso 1: Solicitar entrada al usuario
+
+palabra_ingresada = input ("Por favor, ingrese una palaba: ")
+
+# Paso 2: Contar la cantidad de letras
+
+cant_letras = len(palabra_ingresada)
+
+# Paso 3: Mostrar al usuario el resultado
+
+print("la palabra ingresada tiene", cant_letras, "letras")
